5752c6144558c31ce99fa5356e0cff8f4ddbb95f
justin <justin@localhost>
2015-09-21 10:13:35 -0500

    second commit

--- src/java/com/test/jsf/FormBean.java
+++ src/java/com/test/jsf/FormBean.java
@@ -6,13 +6,17 @@
 package com.test.jsf;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import org.primefaces.event.SelectEvent;
 
 /**
  *
@@ -20,26 +24,28 @@ import javax.faces.event.ActionEvent;
  */
 @ManagedBean
 @SessionScoped
-public class FormBean implements Serializable{
+public class FormBean implements Serializable {
+
     private static final Logger LOG = Logger.getLogger(FormBean.class.getName());
 
     private Form form;
     private People applicant;
-    
+    private boolean addingpersonMode;
+
     /**
      * Creates a new instance of FormBean
      */
     public FormBean() {
     }
-    
+
     @PostConstruct
-    public void gearup(){
+    public void gearup() {
         LOG.log(Level.INFO, "Post Construct Called!!!");
         this.form = new Form();
     }
-    
+
     @PreDestroy
-    public void finishup(){
+    public void finishup() {
         //save form to a file
         //then 
         this.form = null;
@@ -48,23 +54,55 @@ public class FormBean implements Serializable{
     public People getApplicant() {
         return applicant;
     }
-    
-    
-    
-    public void initiateNewPeople(ActionEvent event){
-        //this.form.addPeople(new People());
-        this.applicant = new People();
+
+    public void setApplicant(People applicant) {
+        this.applicant = applicant;
+    }
+
+    public void onDateSelected(SelectEvent event) {
+        LOG.log(Level.INFO, "Made it to onDateSelect");
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
+
+    public void initiateNewPeople(ActionEvent event) {
+        String mode = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("EDT_MODE");
+        LOG.log(Level.INFO, "EDT_MODE parameter value read as  {0}", mode);
+
+        if ("ADD".equalsIgnoreCase(mode)) {
+            //this.form.addPeople(new People());
+            this.applicant = new People();
+            this.addingpersonMode = true;
+        } else {
+            this.addingpersonMode = false;
+        }
     }
     
-    public void commitPeople(ActionEvent event){
-        this.form.addPeople(applicant);
-        this.applicant = null;
+    public void initEdit(ActionEvent event){
+        String id = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("ID");
+        LOG.log(Level.INFO, "Editing applicant with row ID:  {0}", id);
+    }
+
+    public void commitPeople(ActionEvent event) {
+        LOG.log(Level.INFO, "Commit people add or update called");
+        if (this.addingpersonMode) {
+            this.form.addPeople(applicant);
+            this.applicant = null;
+        }
+        this.addingpersonMode = false;
     }
 
     public Form getForm() {
         return form;
     }
-    
-    
-    
+
+    public boolean isAddingpersonMode() {
+        return addingpersonMode;
+    }
+
+    public void setAddingpersonMode(boolean addingpersonMode) {
+        this.addingpersonMode = addingpersonMode;
+    }
+
 }
